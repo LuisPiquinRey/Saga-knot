9dab46db1347b264f5929b37ed4769563ac32001
Luis Piquin Rey <piquin.rey@gmail.com>
2025-09-26 16:25:58 +0200

    feat(product): Created event for product creation

---
+++ product/src/main/java/com/luispiquinrey/product/Command/Event/ProductCreatedEvent.java
@@ -0,0 +1,18 @@
+package com.luispiquinrey.product.Command.Event;
+
+import com.luispiquinrey.product.Enums.Status;
+
+import lombok.Data;
+
+@Data
+public class ProductCreatedEvent {
+    private Long idProduct;
+
+    private String name;
+
+    private String brand;
+
+    private Status status;
+
+    private float price;
+}
