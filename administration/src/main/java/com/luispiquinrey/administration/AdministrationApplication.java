1eae261242c961d2a7e016f80248925317f58734
Luis Piquin Rey <piquin.rey@gmail.com>
2025-09-26 10:03:13 +0200

    fix(administration): Added annotations and actuator dependency

--- administration/src/main/java/com/luispiquinrey/administration/AdministrationApplication.java
+++ administration/src/main/java/com/luispiquinrey/administration/AdministrationApplication.java
@@ -2,8 +2,13 @@ package com.luispiquinrey.administration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
 public class AdministrationApplication {
 
 	public static void main(String[] args) {
