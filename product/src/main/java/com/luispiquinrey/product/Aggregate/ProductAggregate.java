998913e44d0ef1b3744ab2d3c729cd564c47ba50
Luis Piquin Rey <piquin.rey@gmail.com>
2025-09-26 17:54:28 +0200

    feat(product): Added command handler for create product

--- product/src/main/java/com/luispiquinrey/product/Aggregate/ProductAggregate.java
+++ product/src/main/java/com/luispiquinrey/product/Aggregate/ProductAggregate.java
@@ -2,13 +2,17 @@ package com.luispiquinrey.product.Aggregate;
 
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
+import org.springframework.beans.BeanUtils;
 
 import com.luispiquinrey.product.Command.CreateProductCommand;
+import com.luispiquinrey.product.Command.Event.ProductCreatedEvent;
 import com.luispiquinrey.product.Enums.Status;
 
 @Aggregate
 public class ProductAggregate {
+
     @AggregateIdentifier
     private Long productId;
 
@@ -26,7 +30,24 @@ public class ProductAggregate {
     }
 
     @CommandHandler
-    public ProductAggregate(CreateProductCommand createProductCommand){
+    public ProductAggregate(CreateProductCommand createProductCommand) {
+        if (createProductCommand.getName() == null || createProductCommand.getName().isBlank()) {
+            throw new IllegalArgumentException("Product name cannot be null or empty");
+        }
 
+        if (createProductCommand.getBrand() == null || createProductCommand.getBrand().isBlank()) {
+            throw new IllegalArgumentException("Product brand cannot be null or empty");
+        }
+
+        if (createProductCommand.getPrice() < 0) {
+            throw new IllegalArgumentException("Product price cannot be negative");
+        }
+
+        if (createProductCommand.getStock() < 0) {
+            throw new IllegalArgumentException("Product stock cannot be negative");
+        }
+        ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent();
+        BeanUtils.copyProperties(createProductCommand, productCreatedEvent);
+        AggregateLifecycle.apply(productCreatedEvent);
     }
 }
