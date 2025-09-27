9f36e9fb9efe85ad2ca496eb4e35d0abb3cbce8d
Luis Piquin Rey <piquin.rey@gmail.com>
2025-09-27 14:50:51 +0200

    feat(product): Added demo exception handlers for catching exceptions

--- product/src/main/java/com/luispiquinrey/product/Projection/ProductProjection.java
+++ product/src/main/java/com/luispiquinrey/product/Projection/ProductProjection.java
@@ -2,6 +2,7 @@ package com.luispiquinrey.product.Projection;
 
 import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.messaging.interceptors.ExceptionHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -26,5 +27,13 @@ public class ProductProjection {
         Product product=new Product();
         BeanUtils.copyProperties(productCreatedEvent,product);
         repositoryProduct.save(product);
+    }
+    @ExceptionHandler(resultType=Exception.class)
+    public void handle(Exception exception){
+        System.err.println("General exception in ProductProjection: " + exception.getMessage());
+    }
+    @ExceptionHandler(resultType=IllegalArgumentException.class)
+    public void on(IllegalArgumentException exception){
+        System.err.println("IllegalArgumentException in ProductProjection: " + exception.getMessage());
     }
 }
