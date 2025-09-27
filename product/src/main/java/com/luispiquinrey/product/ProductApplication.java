dfe57dcf93ff853769e78b052a9679ff3b3fb908
Luis Piquin Rey <piquin.rey@gmail.com>
2025-09-27 15:04:11 +0200

    feat(product): Autowired interceptor and handler errors

--- product/src/main/java/com/luispiquinrey/product/ProductApplication.java
+++ product/src/main/java/com/luispiquinrey/product/ProductApplication.java
@@ -1,9 +1,15 @@
 package com.luispiquinrey.product;
 
+import org.axonframework.commandhandling.CommandBus;
+import org.axonframework.config.EventProcessingConfigurer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
+
+import com.luispiquinrey.product.Configuration.CommandInterceptor;
+import com.luispiquinrey.product.Configuration.EventErrorHandler;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +17,13 @@ public class ProductApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApplication.class, args);
+	}
+	@Autowired
+	public void registerInterceptorCommandProduct(ApplicationContext context, CommandBus commandBus){
+		commandBus.registerDispatchInterceptor(context.getBean(CommandInterceptor.class));
+	}
+	@Autowired
+	public void configure(EventProcessingConfigurer config){
+		config.registerListenerInvocationErrorHandler("product-collection", conf-> new EventErrorHandler());
 	}
 }
