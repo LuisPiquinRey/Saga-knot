5b503ec0c085d375c0467d7eb574a26bf26b01ca
Luis Piquin Rey <piquin.rey@gmail.com>
2025-09-30 19:44:45 +0200

    feat(order): Added repository

---
+++ parent/order/src/main/java/com/luispiquinrey/order/Repository/RepositoryOrder.java
@@ -0,0 +1,9 @@
+package com.luispiquinrey.order.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luispiquinrey.order.Entities.Order;
+
+public interface RepositoryOrder extends JpaRepository<Order, String>{
+    
+}
