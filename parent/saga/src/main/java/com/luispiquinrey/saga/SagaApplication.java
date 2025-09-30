51bf1efc157ab3839f8d46da5eb08619b3882d89
Luis Piquin Rey <piquin.rey@gmail.com>
2025-09-30 19:13:18 +0200

    feat: Added new package saga for managing transictions

---
+++ parent/saga/src/main/java/com/luispiquinrey/saga/SagaApplication.java
@@ -0,0 +1,13 @@
+package com.luispiquinrey.saga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagaApplication.class, args);
+	}
+
+}
