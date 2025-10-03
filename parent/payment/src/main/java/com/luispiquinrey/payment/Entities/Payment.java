48ee9f2ba28ddc5f990d8e8a3185a4259b9169ac
Luis Piquin Rey <piquin.rey@gmail.com>
2025-10-03 19:21:31 +0200

    feat(payment): Added entity payment

---
+++ parent/payment/src/main/java/com/luispiquinrey/payment/Entities/Payment.java
@@ -0,0 +1,71 @@
+package com.luispiquinrey.payment.Entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="payment")
+public class Payment {
+    @Id
+    private String idPayment;
+    private String provider;
+    private LocalDate expiryDate;
+    private long accountNumber;
+    private int CSV;
+
+    
+
+    public Payment(int CSV, long accountNumber, LocalDate expiryDate, String provider) {
+        this.CSV = CSV;
+        this.accountNumber = accountNumber;
+        this.expiryDate = expiryDate;
+        this.provider = provider;
+    }
+
+    public Payment() {
+    }
+
+    public String getIdPayment() {
+        return idPayment;
+    }
+
+    public void setIdPayment(String idPayment) {
+        this.idPayment = idPayment;
+    }
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public void setProvider(String provider) {
+        this.provider = provider;
+    }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getCSV() {
+        return CSV;
+    }
+
+    public void setCSV(int CSV) {
+        this.CSV = CSV;
+    }
+
+}
