4cae19cb4945202cd4fc60f4409ab6fc5d1ef640
LuisPiquinRey <piquin.rey@gmail.com>
2025-10-08 19:43:21 +0200

    feat(configs): Added amqp bus

--- configs/src/main/java/com/luispiquinrey/configs/ConfigsApplication.java
+++ configs/src/main/java/com/luispiquinrey/configs/ConfigsApplication.java
@@ -3,9 +3,11 @@ package com.luispiquinrey.configs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableConfigServer
+@RefreshScope
 public class ConfigsApplication {
 
 	public static void main(String[] args) {
