afde01d0fba684f8780122e911d40f8f30a4bc5f
Luis Piquin Rey <piquin.rey@gmail.com>
2025-10-09 18:03:16 +0200

    style(view): Added header of the home shop

---
+++ view/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='header'>
+            <img src='menu.svg'/>
+            <h1>Knot shop</h1>
+            <img src='search.svg'/>
+        </div>
+    );
+};
+
+export default Home;
--- view/src/main.jsx
+++ view/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import Home from './components/Home';
+import './css/Home.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Home/>
   </StrictMode>,
 )
