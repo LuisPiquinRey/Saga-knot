cc962ead4af967546c8817e67ba28906210d5349
Luis Piquin Rey <piquin.rey@gmail.com>
2025-10-10 11:25:22 +0200

    feat(view): Create carousel

---
+++ view/src/components/Carousel.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Carousel = () => {
+    return (
+        <div id="carouselExample" className="carousel slide">
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img src="caroussel-image.jpg" className="carousel" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="caroussel-image-second.jpg" className="carousel" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="caroussel-image-third.jpg" className="carousel" alt="..." />
+                </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+        </div>
+    );
+}
+
+export default Carousel;
--- view/src/main.jsx
+++ view/src/main.jsx
@@ -4,10 +4,12 @@ import Header from './components/Header';
 import './css/Header.css';
 import Footer from './components/Footer';
 import './css/Footer.css'
+import Carousel from './components/Carousel';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header/>
     <Footer/>
+    <Carousel/>
   </StrictMode>,
 )
