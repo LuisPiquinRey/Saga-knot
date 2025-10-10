0341184d8a72fa7218e8c8e0cae41e4f1c4b379b
Luis Piquin Rey <piquin.rey@gmail.com>
2025-10-10 11:15:34 +0200

    chore(view): Added bootstrap

--- view/src/components/Header.jsx
+++ view/src/components/Header.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <>
-            <header className='header'>
-                <img src='menu.svg'/>
-                <h1>Knot shop</h1>
-                <img src='search.svg'/>
-            </header>
-        </>
+        <header className="header">
+            <img src="menu.svg" />
+            <h1>Knot shop</h1>
+            <div className="right-items">  
+                <img src="search.svg" />
+                <img src="person.svg" />
+                <img src="cart.svg" />
+            </div>
+        </header>
     );
 };
+
 
 export default Header;
