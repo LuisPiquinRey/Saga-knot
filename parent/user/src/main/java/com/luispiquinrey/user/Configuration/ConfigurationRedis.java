56b9e226a74a0a53b9f97dd331a7d7b49751dba0
Luis Piquin Rey <piquin.rey@gmail.com>
2025-10-10 16:02:52 +0200

    feat(docker): Added redis replication with sentinel

---
+++ parent/user/src/main/java/com/luispiquinrey/user/Configuration/ConfigurationRedis.java
@@ -0,0 +1,17 @@
+package com.luispiquinrey.user.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+
+
+@Configuration
+public class ConfigurationRedis {
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("server", 6379));
+    }
+}
