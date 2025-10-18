0733db44125dffbbc90554b4e4e6225facd6e00d
Luis Piquin Rey <piquin.rey@gmail.com>
2025-10-18 17:52:26 +0200

    feat(user): Added envoiranment with docker for mysql testing

--- parent/user/src/test/java/com/luispiquinrey/user/Containers/MysqlTestContainer.java
+++ parent/user/src/test/java/com/luispiquinrey/user/Containers/MysqlTestContainer.java
@@ -4,8 +4,18 @@ import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-
+@TestConfiguration(proxyBeanMethods = false)
+public class MysqlTestContainer{
+    @Bean
+    @ServiceConnection
+    MySQLContainer<?> mysqlContainer(){
+        return new MySQLContainer<>(DockerImageName.parse("mysql:oraclelinux9")); 
+    }
+}
--- parent/user/src/test/java/com/luispiquinrey/user/Repository/ContactRepositoryTest.java
+++ parent/user/src/test/java/com/luispiquinrey/user/Repository/ContactRepositoryTest.java
@@ -13,9 +13,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import com.luispiquinrey.user.Containers.MysqlTestContainer;
 import com.luispiquinrey.user.Entities.Contact;
 
 @DataJpaTest
+@Import(MysqlTestContainer.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class ContactRepositoryTest {
     @Autowired
     private ContactRepository contactRepository;
