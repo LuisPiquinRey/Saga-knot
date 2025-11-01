3e99d54ebf065307ec2089afe085efe4e4e40ffe
LuisPiquinRey <piquin.rey@gmail.com>
2025-11-01 10:30:06 +0100

    feat(product): Added category

---
+++ parent/product/src/main/java/com/luispiquinrey/product/Entities/Category.java
@@ -0,0 +1,78 @@
+package com.luispiquinrey.product.Entities;
+
+import java.util.UUID;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import java.util.UUID;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @Column(name = "id_category", updatable = false, nullable = false)
+    private String idCategory = UUID.randomUUID().toString();
+
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+
+    @Size(max = 255, message = "Description cannot exceed 255 characters")
+    private String description;
+
+    @Size(max = 255, message = "Image URL cannot exceed 255 characters")
+    private String image;
+
+    public Category() {}
+
+    public Category(String name, String description, String image) {
+        this.idCategory = UUID.randomUUID().toString();
+        this.name = name;
+        this.description = description;
+        this.image = image;
+    }
+
+    public String getIdCategory() {
+        return idCategory;
+    }
+
+    public void setIdCategory(String idCategory) {
+        this.idCategory = idCategory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "idCategory='" + idCategory + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", image='" + image + '\'' +
+                '}';
+    }
+}
