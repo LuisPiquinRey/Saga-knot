91d9fb9a4689cf062f97d68099d3f74458d387c8
Luis Piquin Rey <piquin.rey@gmail.com>
2025-11-04 19:07:16 +0100

    style(view): Added buttons for menu

--- view/src/components/Main.jsx
+++ view/src/components/Main.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 export function Main() {
     return (
-        <h1 className="title">Reinventing the way <br/> you buy and 
-            <span className="text-gradient"> win</span></h1>
+        <div className="main-block">
+            <h1 className="title">
+                Reinventing the way <br /> you buy and
+                <span className="text-gradient"> win</span>
+            </h1>
+            <div className="collection-buttons">
+                <button className="button">Get started</button>
+                <button className="button-transparent">Book a demo</button>
+            </div>
+        </div>
     );
 }
