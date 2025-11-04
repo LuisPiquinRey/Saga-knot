aa53536d91c8e742ce1659d3b9b7be6ebc77f31b
Luis Piquin Rey <piquin.rey@gmail.com>
2025-11-04 16:56:56 +0100

    feat(view): Start with main item

---
+++ view/src/components/Main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+export function Main() {
+    return (
+        <h1>Hola react</h1>
+    );
+}
--- view/src/main.jsx
+++ view/src/main.jsx
@@ -1,17 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import Header from './components/Header';
-import './css/Header.css';
-import Footer from './components/Footer';
-import './css/Footer.css'
-import Carousel from './components/Carousel';
-import Main from './components/Main';
+import { Main } from './components/Main';
+import './css/index.css';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header/>
-    <Footer/>
-    <Carousel/>
     <Main/>
   </StrictMode>,
 )
