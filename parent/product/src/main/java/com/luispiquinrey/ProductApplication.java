b9e96905e9de448d8ecfd93f3aeac0ed3f3553e9
Luis Piquin Rey <piquin.rey@gmail.com>
2025-12-27 20:01:23 +0100

    fix(product): Upgrade axon version!

---
+++ parent/product/src/main/java/com/luispiquinrey/ProductApplication.java
@@ -0,0 +1,29 @@
+package com.luispiquinrey.product;
+
+import org.axonframework.commandhandling.CommandBus;
+import org.axonframework.config.EventProcessingConfigurer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.luispiquinrey.product.Configuration.EventErrorHandler;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(basePackages = {"com.luispiquinrey.Error.Handler", "com.luispiquinrey.product.*"})
+@EnableJpaAuditing
+public class ProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
+    @Autowired
+    public void configure(EventProcessingConfigurer config){
+        config.registerListenerInvocationErrorHandler("product-collection", conf-> new EventErrorHandler());
+    }
+}
--- parent/product/src/main/java/com/luispiquinrey/product/Configuration/AxonConfig.java
+++ parent/product/src/main/java/com/luispiquinrey/product/Configuration/AxonConfig.java
@@ -1,6 +1,7 @@
 package com.luispiquinrey.product.Configuration;
 
 import com.thoughtworks.xstream.XStream;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.axonframework.commandhandling.CommandBus;
 import org.axonframework.commandhandling.CommandMessage;
 import org.axonframework.config.Configurer;
@@ -10,6 +11,7 @@ import org.axonframework.eventsourcing.SnapshotTriggerDefinition;
 import org.axonframework.eventsourcing.Snapshotter;
 import org.axonframework.eventsourcing.snapshotting.SnapshotFilter;
 import org.axonframework.messaging.interceptors.BeanValidationInterceptor;
+import org.axonframework.micrometer.GlobalMetricRegistry;
 import org.axonframework.serialization.Serializer;
 import org.axonframework.serialization.xml.XStreamSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
